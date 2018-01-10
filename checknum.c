835005eece3e63f8b5a991654de0b65953688db3
shrivastavaayushi <35270455+shrivastavaayushi@users.noreply.github.com>
2018-01-10 21:57:31 +0530

    Create checknum.c

---
+++ checknum.c
@@ -0,0 +1,13 @@
+#include<stdio.h
+void main()
+{
+ int n;
+ printf("Enter a number");
+ scanf("%d",&n);
+ if(n<0)
+ printf("Negative Number");
+ else if (n>0)
+ printf("Positive Number");
+ else
+ printf("Zero");
+ }
